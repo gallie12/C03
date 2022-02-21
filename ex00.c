e2073dea090807323b55559701d5c3201c63ebd0
hannah azan <hannahazan@hotmail.fr>
2022-02-21 19:12:18 +0100

    myfirstcommit

---
+++ ex00.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include<stdio.h>
+#include<string.h>
+
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
---
+++ ex01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int		ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && (i < n - 1) && s1[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+
+
